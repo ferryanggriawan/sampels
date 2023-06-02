91f0410218318766b59bb4eff77e6f15dce22027
ferryanggriawan <ferrya1997@gmail.com>
2023-06-02 13:37:40 +0700

    fix name

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ arg="$1"
 
 if [ $arg = "dev" ]; then
   echo "compiling without compressed output"
-  sass sass/main.scss dist/samples.css --watch --no-source-map
+  sass sass/main.scss dist/sampels.css --watch --no-source-map
 elif [ $arg = "build:dev" ]; then
   echo "compiling with compressed output"
-  sass sass/main.scss dist/samples.min.css --style=compressed --watch --no-source-map
+  sass sass/main.scss dist/sampels.min.css --style=compressed --watch --no-source-map
 elif [ $arg = "build" ]; then
   echo "compiling with compressed output"
-  sass sass/main.scss dist/samples.min.css --style=compressed --no-source-map
+  sass sass/main.scss dist/sampels.min.css --style=compressed --no-source-map
 else
   echo "argument should be one of this [dev | build:dev | build]"
 fi
